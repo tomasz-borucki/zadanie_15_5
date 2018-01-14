e566e9520c81c75cca9313ebf2a9aea6cdbf8f8b
Your Name <you@example.com>
2018-01-14 22:25:13 +0100

    Add files to project

---
+++ components/App.js
@@ -0,0 +1,75 @@
+var GIPHY_API_URL = 'http://api.giphy.com';
+var GIPHY_PUB_KEY = 'QvY7f51VFj8CoLD1bhyEXjY8csjxi8GQ'
+
+App = React.createClass({
+	getInitialState() {
+		return {
+				loading: false,
+				searchingText: '',
+				gif: {}
+		};
+	},
+
+	getGif: function(searchingText) {
+		return new Promise(function(resolve, reject) {
+			var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url);
+			xhr.onload = function() {
+				if (xhr.status === 200) {
+					var data = JSON.parse(xhr.responseText).data;
+					var gif = {
+						url: data.fixed_width_downsampled_url,
+						sourceUrl: data.url
+					};
+					
+					resolve(gif);
+				} else {
+					reject(new Error(xhr.statusText));
+				}
+			};
+			xhr.onerror = function () {
+				reject(new Error(`XMLHttpRequest Error: ${xhr.statusText}`));
+			};
+
+			xhr.send();
+		})
+	},
+
+	handleSearch: function(searchingText) {
+		this.setState({
+			loading: true
+		});
+		this.getGif(searchingText)
+			.then((gif) => {
+				this.setState({
+					loading: false,
+					gif: gif,
+					searchingText: searchingText
+				})
+			}	
+		)
+	},
+  
+	render: function() {
+
+		var styles = {
+			margin: '0 auto',
+			textAlign: 'center',
+			width: '90%'
+		};
+
+		return (
+		<div style={styles}>
+				<h1>Wyszukiwarka GIFow!</h1>
+				<p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+				<Search onSearch={this.handleSearch} />
+			<Gif 
+				loading={this.state.loading}
+				url={this.state.gif.url}
+				sourceUrl={this.state.gif.sourceUrl}
+			/>
+		</div>
+		);
+	}
+});
